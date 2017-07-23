dd21f4e6ea8d20ca5dabf6076feab11b16a89778
dtying <dingtingying@gmail.com>
2017-07-23 17:51:22 +0800

    demo 1 - create a simple react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+	render() {
+		return(
+		<div>
+			<h1>Hello React app is running</h1>
+		</div>
+		);
+	}
+}
+
+export default App;
